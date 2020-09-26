60c2bd9f4aceeff588eb82694d4d0b2ecf4b96c2
janwrob120 <janwrob120@gmail.com>
2020-09-26 12:38:04 +0200

    Got back to 1884 port, because 1883 is alreaddy in use.

--- env/Src/controller.py
+++ env/Src/controller.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sensors_data.db' # new
 
 app.config['MQTT_BROKER_URL'] = '192.168.1.110'  # use the free broker from HIVEMQ
-app.config['MQTT_BROKER_PORT'] = 1883  # default port for mqtt connection
+app.config['MQTT_BROKER_PORT'] = 1884  # default port for non-tlp connection
 app.config['MQTT_USERNAME'] = ''  # set the username here if you need authentication for the broker
 app.config['MQTT_PASSWORD'] = ''  # set the password here if the broker demands authentication
 app.config['MQTT_KEEPALIVE'] = 20  # set the time interval for sending a ping to the broker to 20 seconds
